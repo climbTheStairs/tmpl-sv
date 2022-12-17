eae2b60596125ca61517ab3c691231c223e6a768
climbTheStairs <yxtqwf+github@gmail.com>
2022-12-17 15:17:57 -0800

    read.py: document

--- read.py
+++ read.py
@@ -1,4 +1,6 @@
-from os import sys
+# This program does not actually do anything.
+# I just made it for fun to see
+# how short I could rewrite read.go in Python.
 
 def read_tsv(f):
     head = f.readline()[:-1].split("\t")
