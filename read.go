93481f3475dd4b16071b8d5325484591c7f1afa5
climbTheStairs <yxtqwf+github@gmail.com>
2022-12-18 13:53:54 -0800

    read.go: move escaping out of createRow, make new fn escapeTable

--- read.go
+++ read.go
@@ -10,11 +10,19 @@ import (
 )
 
 func main() {
-	esc := true
-	table, err := readTsv(os.Stdin, esc)
+	table, head, err := readTsv(os.Stdin)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(1)
+	}
+	if esc := true; esc {
+		t, err := escapeTable(table, head)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr,
+				"%s: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+		table = t
 	}
 	if err := applyTmpl(os.Args[1], table, os.Stdout); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
@@ -34,47 +42,55 @@ func scanPosixLines(d []byte, atEOF bool) (int, []byte, error) {
 	return 0, nil, nil
 }
 
-func readTsv(f io.Reader, esc bool) ([]map[string]string, error) {
+func readTsv(f io.Reader) ([]map[string]string, []string, error) {
 	scanner := bufio.NewScanner(f)
 	scanner.Split(scanPosixLines)
 
 	if ok := scanner.Scan(); !ok {
-		return nil, fmt.Errorf("cannot read initial line")
+		return nil, nil, fmt.Errorf("cannot read initial line")
 	}
 	head := strings.Split(scanner.Text(), "\t")
 
-	table := make([]map[string]string, 0)
+	t := make([]map[string]string, 0)
 	for lnum := 2; scanner.Scan(); lnum++ {
-		row, err := createRow(scanner.Text(), head, esc)
+		row, err := createRow(scanner.Text(), head)
 		if err != nil {
-			return table, fmt.Errorf("line %d: %v\n", lnum, err)
+			return t, head, fmt.Errorf("line %d: %v", lnum, err)
 		}
-		table = append(table, row)
+		t = append(t, row)
 	}
-	return table, nil
+	return t, head, nil
 }
 
-func createRow(s string, head []string, esc bool) (map[string]string, error) {
+func createRow(s string, head []string) (map[string]string, error) {
 	cols := strings.Split(s, "\t")
 	if len(cols) != len(head) {
 		return nil, fmt.Errorf("invalid number of columns")
 	}
 	row := make(map[string]string)
 	for i, v := range head {
-		col, err := escapeIf(cols[i], esc)
-		if err != nil {
-			return nil, fmt.Errorf(`column %d "%s": %v`, i, v, err)
-		}
-		row[v] = col
+		row[v] = cols[i]
 	}
 	return row, nil
 }
 
-// escapeIf wraps escape, returning s
-// escaped if esc is true, unchanged otherwise.
-func escapeIf(s string, esc bool) (string, error) {
-	if esc {
-		return escape(s)
+func escapeTable(t []map[string]string, head []string) ([]map[string]string, error) {
+	for i := range t {
+		var err error
+		lnum := i + 2
+		if t[i], err = escapeRow(t[i], head); err != nil {
+			return t, fmt.Errorf("line %d: %v", lnum, err)
+		}
 	}
-	return s, nil
+	return t, nil
+}
+
+func escapeRow(row map[string]string, head []string) (map[string]string, error) {
+	for i, k := range head {
+		var err error
+		if row[k], err = escape(row[k]); err != nil {
+			return row, fmt.Errorf(`column %d "%s": %v`, i, k, err)
+		}
+	}
+	return row, nil
 }
