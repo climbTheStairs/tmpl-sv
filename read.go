6a8500d267d33766c857b1f82618cb9049b8166c
climbTheStairs <yxtqwf+github@gmail.com>
2022-12-17 20:25:27 -0800

    read.go: make separate createRow fn

--- read.go
+++ read.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -39,23 +38,29 @@ func readTsv(f io.Reader) ([]map[string]string, error) {
 	scanner.Split(scanPosixLines)
 
 	if ok := scanner.Scan(); !ok {
-		return nil, errors.New("cannot read initial line")
+		return nil, fmt.Errorf("cannot read initial line")
 	}
 	head := strings.Split(scanner.Text(), "\t")
 
 	table := make([]map[string]string, 0)
 	for lnum := 2; scanner.Scan(); lnum++ {
-		cols := strings.Split(scanner.Text(), "\t")
-		if len(cols) != len(head) {
-			return table, fmt.Errorf("line %d contains "+
-				"invalid number of columns", lnum)
-		}
-		row := make(map[string]string)
-		for i, colName := range head {
-			row[colName] = cols[i]
+		row, err := createRow(scanner.Text(), head)
+		if err != nil {
+			return table, fmt.Errorf("line %d: %v\n", lnum, err)
 		}
 		table = append(table, row)
 	}
-
 	return table, nil
+}
+
+func createRow(s string, head []string) (map[string]string, error) {
+	cols := strings.Split(s, "\t")
+	if len(cols) != len(head) {
+		return nil, fmt.Errorf("invalid number of columns")
+	}
+	row := make(map[string]string)
+	for i, colName := range head {
+		row[colName] = cols[i]
+	}
+	return row, nil
 }
