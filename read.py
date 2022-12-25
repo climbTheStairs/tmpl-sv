5a451f33a90ee1dfb960fe826e6f087471579716
climbTheStairs <yxtqwf+github@gmail.com>
2022-12-25 00:33:56 -0800

    read.py: rm

--- read.py
+++
@@ -1,9 +0,0 @@
-# This program does not actually do anything.
-# I just made it for fun to see
-# how short I could rewrite read.go in Python.
-
-def read_tsv(f):
-    head = f.readline()[:-1].split("\t")
-    table = [{head[i]: v for i, v in enumerate(l[:-1].split("\t"))} for l in f]
-    return table
-
